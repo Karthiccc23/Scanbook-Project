0806a2f14c55c22a8b9e4ab2bda69c4b672fb87b
Karthiccc23 <karthiccc23@gmail.com>
2016-11-18 13:38:03 -0800

    Update BookDetailsImpl.java

--- Back-end/Scanbook-server/main/java/scanbook/user/databaseservice/BookDetailsImpl.java
+++ Back-end/Scanbook-server/main/java/scanbook/user/databaseservice/BookDetailsImpl.java
@@ -180,6 +180,46 @@ public class BookDetailsImpl implements BookDetailsinterface {
 	}
 	
 	
+	
+	public boolean Servervalidation( String isbn )
+	{
+		if ( isbn == null )
+		{
+			return false;
+		}
+
+		isbn = isbn.replaceAll( "-", "" );
+
+		if ( isbn.length() != 13 )
+		{
+			return false;
+		}
+
+		try
+		{
+			int tot = 0;
+			for ( int i = 0; i < 12; i++ )
+			{
+				int digit = Integer.parseInt( isbn.substring( i, i + 1 ) );
+				tot += (i % 2 == 0) ? digit * 1 : digit * 3;
+			}
+
+			int checksum = 10 - (tot % 10);
+			if ( checksum == 10 )
+			{
+				checksum = 0;
+			}
+
+			return checksum == Integer.parseInt( isbn.substring( 12 ) );
+		}
+		catch ( NumberFormatException nfe )
+		{
+			return false;
+		}
+	}
+
+	
+	
 
 }
 
